dcbd7bb97ce778075a61dcd9ea59da54c6914cf3
James Mullaney <james@ht2.co.uk>
2014-02-05 17:14:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# BadgeGrabber
+
+###v0.1
+
+A jquery function for finding assertions and handling them with a callback
+
---
+++ js/plugins.js
@@ -0,0 +1,37 @@
+var App =  ( typeof App === 'undefined' ) ? {} : App;
+
+/**
+ * badgeGetter - retrieves the OpenBadge JSON assertion for a badge
+ * @param  {jquery Object}   	$selector 					The jquery selector to find assertions in
+ * @param  {string}						assertionAttribute 	The attribute name containing the assertion (e.g. data-url, href, rel)
+ * @param  {Function} 				callback           	The function to call for every assertion collected
+ *                                            		returns: 
+ *                                            							data - the badge assertion
+ *                                            							url - the assertion url
+ *                                            							$element - the original jquery Object the assertion was taken from
+ * @param  {Function}   			finishCallback     	A function called when all assertions have been collected
+ */
+App.badgeGetter = function( $selector, assertionAttribute, callback, finishCallback ){
+	var completed = 0;
+	$selector.each( function(){
+		var $el = $(this);
+		
+		if( typeof $el.attr(assertionAttribute) !== 'undefined' ){
+			var assertion_url = $el.attr(assertionAttribute);
+
+			$.getJSON( assertion_url, function(data){
+				callback(data, assertion_url, $el);
+
+				completed++;
+				if( completed == $selector.length ){
+					finishCallback();
+				}
+			}).error( function(e){
+				//console.log("Error getting assertion");
+			});
+
+		} else {
+			//console.log("Attr '"+assertionAttribute+"' not found");   
+		}								
+	});
+};
